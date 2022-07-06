5d8891fe7798995fee9b6f7cf4e11f9a1c5b2201
Kacper Neumann <kapilek31@gmail.com>
2022-07-06 15:11:56 +0200

    Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {}
+		screens: {
+			sm: '480px',
+			md: '768px',
+			lg: '976px',
+			xl: '1440px'
+		},
+		extend: {
+			colors: {
+				primary: '#202c39',
+				'primary-light': '#283845',
+				'primary-dark': '#1d2834',
+				secondary: '#f2c584',
+				'secondary-light': '#f2d492',
+				'secondary-dark': '#f2b576'
+			}
+		}
 	},
 	plugins: []
 };
