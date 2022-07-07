5919f9b82b3bdf5c1aa8d8d292d84efd8bf88a08
Kacper Neumann <kapilek31@gmail.com>
2022-07-07 15:54:11 +0200

    Created /about route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Summoner from './pages/Summoner';
+import About from './pages/About';
 import NotFound from './pages/NotFound';
+import Navbar from './components/layout/Navbar';
 
 function App() {
 	return (
 		<Router>
+			<Navbar />
 			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='summoner/:region/:summonerName' element={<Summoner />} />
-				<Route path='*' element={<NotFound />} />
+				<Route path="/" element={<Home />} />
+				<Route path="summoner/:region/:summonerName" element={<Summoner />} />
+				<Route path="about" element={<About />} />
+				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</Router>
 	);
