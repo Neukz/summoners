ec02ee8f0c6beb82d91fbd60da9ae6a2c17dcddb
Kacper Neumann <kapilek31@gmail.com>
2022-07-12 00:15:19 +0200

    Created regions constants

---
+++ src/constants/regions.js
@@ -0,0 +1,58 @@
+// Game regions mapped to their respective game IDs used in the API
+export const regions = [
+	{
+		name: 'North America',
+		abbreviation: 'NA',
+		code: 'NA1'
+	},
+	{
+		name: 'Europe West',
+		abbreviation: 'EUW',
+		code: 'EUW1'
+	},
+	{
+		name: 'Europe Nordic & East',
+		abbreviation: 'EUNE',
+		code: 'EUN1'
+	},
+	{
+		name: 'Republic of Korea',
+		abbreviation: 'KR',
+		code: 'KR'
+	},
+	{
+		name: 'Brazil',
+		abbreviation: 'BR',
+		code: 'BR1'
+	},
+	{
+		name: 'Japan',
+		abbreviation: 'JP',
+		code: 'JP1'
+	},
+	{
+		name: 'Russia',
+		abbreviation: 'RU',
+		code: 'RU1'
+	},
+	{
+		name: 'Oceania',
+		abbreviation: 'OCE',
+		code: 'OC1'
+	},
+	{
+		name: 'Turkey',
+		abbreviation: 'TR',
+		code: 'TR1'
+	},
+	{
+		name: 'Latin America North',
+		abbreviation: 'LAN',
+		code: 'LA1'
+	},
+	{
+		name: 'Latin America South',
+		abbreviation: 'LAS',
+		code: 'LA2'
+	}
+];
