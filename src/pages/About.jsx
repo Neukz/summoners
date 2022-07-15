979cc0c8c8a1ce1695ae52072bbd09046faae3ad
Kacper Neumann <kapilek31@gmail.com>
2022-07-15 18:04:39 +0200

    Changed text formatting

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,18 @@
 const About = () => {
 	return (
-		<div className="divide-y divide-slate-500 text-xl sm:text-justify">
-			<h1 className="py-4 text-center text-2xl sm:text-left md:text-3xl lg:text-4xl">
-				About
-			</h1>
+		<div className="divide-y divide-slate-500 text-lg text-justify sm:text-xl ">
+			<header className="py-4">
+				<h1 className="text-center text-2xl md:text-left md:text-3xl lg:text-4xl">
+					About
+				</h1>
+			</header>
+
 			<p className="py-4">
 				Summoners is a web application that allows League of Legends and
 				Teamfight Tactics players to track their in-game performance and compare
 				them to other players.
 			</p>
+
 			<p className="py-4">
 				Summoners is a personal and open source project created by Kacper
 				Neumann. The app uses author's{' '}
@@ -23,6 +27,7 @@ const About = () => {
 				This website utilizes CommunityDragon as well, which provides game
 				assets for the application.
 			</p>
+
 			<p className="py-4">
 				Summoners was created under Riot Games'{' '}
 				<a
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,13 @@ const Home = () => {
 	return (
 		<>
 			<div className="flex justify-center items-center md:flex-col">
-				<h1 className="hidden text-2xl sm:block md:text-3xl lg:text-4xl">
-					<span className="text-secondary">LoL</span> &amp;{' '}
-					<span className="text-secondary">TFT</span> stats for your favorite{' '}
-					<span className="text-secondary">Summoners</span>.
-				</h1>
+				<header className="hidden sm:block">
+					<h1 className="text-2xl md:text-3xl lg:text-4xl">
+						<span className="text-secondary">LoL</span> &amp;{' '}
+						<span className="text-secondary">TFT</span> stats for your favorite{' '}
+						<span className="text-secondary">Summoners</span>.
+					</h1>
+				</header>
 
 				<img
 					src="https://raw.communitydragon.org/latest/game/assets/loadouts/summoneremotes/champions/leesin/leesin_happy_cheers_vfx.png"
