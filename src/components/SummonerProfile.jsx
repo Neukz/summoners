f49637279fab5f6f37b757f913d1458fcf3d1a8a
Kacper Neumann <kapilek31@gmail.com>
2022-07-28 23:13:54 +0200

    Separate component with Summoner icon

---
+++ src/components/SummonerProfile.jsx
@@ -0,0 +1,25 @@
+import { useContext } from 'react';
+
+import RiotContext from '../context/riot/RiotContext';
+
+const SummonerProfile = () => {
+	const { summoner } = useContext(RiotContext);
+
+	return (
+		<div className="flex flex-col items-center rounded-xl p-2 text-center">
+			<img
+				className="w-24 rounded-full"
+				src={`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/profile-icons/${summoner.profileIconId}.jpg`}
+				alt=""
+			/>
+
+			<h2 className="text-lg font-bold">{summoner.name}</h2>
+
+			<div className="text-slate-400 text-sm">
+				Level {summoner.summonerLevel}
+			</div>
+		</div>
+	);
+};
+
+export default SummonerProfile;
--- src/pages/Summoner.jsx
+++ src/pages/Summoner.jsx
@@ -1,16 +1,16 @@
 import { useEffect, useContext } from 'react';
 import { useParams } from 'react-router-dom';
+import SummonerProfile from '../components/SummonerProfile';
 import QueueCard from '../components/QueueCard';
 
 import RiotContext from '../context/riot/RiotContext';
 
 const Summoner = () => {
-	const { getSummoner, clearSummoner, summoner, LoLStats, TFTStats } =
+	const { getSummoner, clearSummoner, LoLStats, TFTStats } =
 		useContext(RiotContext);
 	const { region, summonerName } = useParams();
 
 	useEffect(() => {
-		console.log(region, summonerName);
 		getSummoner(region, summonerName);
 
 		// Clear summoner data on unmount
@@ -21,19 +21,7 @@ const Summoner = () => {
 
 	return (
 		<>
-			<div className="flex flex-col items-center rounded-xl p-2 text-center">
-				<img
-					className="w-24 rounded-full"
-					src={`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/profile-icons/${summoner.profileIconId}.jpg`}
-					alt=""
-				/>
-
-				<h2 className="text-lg font-bold">{summoner.name}</h2>
-
-				<div className="text-slate-400 text-sm">
-					Level {summoner.summonerLevel}
-				</div>
-			</div>
+			<SummonerProfile />
 
 			<div className="flex flex-col flex-wrap justify-center items-center gap-2 md:flex-row">
 				{/* LoL ranking */}
