677824355e98b677a8c83bf574e4958173fa5303
Kacper Neumann <kapilek31@gmail.com>
2022-07-29 23:52:13 +0200

    SET_ERROR and CLEAR_ERROR

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -2,5 +2,12 @@
 
 // Get summoner account data and stats for both games
 export const GET_SUMMONER = 'GET_SUMMONER';
+
 // Clear summoner data from state
 export const CLEAR_SUMMONER = 'CLEAR_SUMMONER';
+
+// Set error
+export const SET_ERROR = 'SET_ERROR';
+
+// Clear error
+export const CLEAR_ERROR = 'CLEAR_ERROR';
